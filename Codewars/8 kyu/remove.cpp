01fbf03be1244cb9562c588e65e22790cabb6d21
amxnese <aminesdrt42@gmail.com>
2023-11-07 16:47:15 +0000

    Added 'remove.cpp' File

---
+++ Codewars/8 kyu/remove.cpp
@@ -0,0 +1,22 @@
+/*
+It's pretty straightforward. Your goal is to create a function that removes
+the first and last characters of a string. You're given one parameter, 
+the original string. You don't have to worry with strings with less than 
+two characters.
+*/
+#include <string>
+using namespace std; 
+
+// My Solution
+string sliceString (string str )
+{
+    string result = "";
+    for(int i = 1;i < str.length()-1;i++){
+        result += str[i];
+    }return result;
+}
+
+// Best Practice
+string sliceString (string str ){
+    return str.substr(1, str.size() - 2); 
+}
